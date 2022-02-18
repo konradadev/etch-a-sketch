05bbb400b90ecd5e4eede741bfb793838f944c5e
konradadev <konradadev@gmail.com>
2022-02-18 16:28:31 +0100

    Add buttons to clear or resize board

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 //Etch-a-Sketch script
+function changeColorDown(e){
+    if(mouseDown===false){
+        e.target.style.setProperty("background-color", "black");
+        e.target.setAttribute("class", "cellchosen");
+    }
+}
+
+function changeColorOver(e){
+    e.stopPropagation();
+    if(mouseDown===true){
+        e.target.style.setProperty("background-color", "black");
+        e.target.setAttribute("class", "cellchosen");
+    }else{
+        changeColorHover(e);
+    }
+}
+
+function changeColorHover(e){
+    if(e.target.getAttribute("class")!=="cellchosen"){
+        e.target.setAttribute("class", "cell cellhover")
+    }
+}
+function changeColorBackHover(e){
+    if(e.target.getAttribute("class")!=="cellchosen"){
+        e.target.setAttribute("class", "cell")
+    }
+}
+
 function createCells(numberOfCells){
-    clearBoard(currentCells);
+    clearBoard();
     const container = document.querySelector(".container");
     for(let i=0; i<numberOfCells*numberOfCells; i++){
         const cell = document.createElement("div");
         cell.setAttribute("class", "cell");
-        cell.addEventListener("mouseover", (e) => {
-            if(mouseDown===true){
-                e.target.style.setProperty("background-color", "black");
-            }
-        });
+        cell.removeEventListener("pointercancel", PointerEvent);
+        cell.addEventListener("mousedown", changeColorDown);
+        cell.addEventListener("mouseenter", changeColorOver);
+        cell.addEventListener("mouseleave", changeColorBackHover);
+        cell.addEventListener("mouseup", ()=>{mouseDown=false});
         container.appendChild(cell);
     }
     container.style.setProperty("grid-template-columns", `repeat(${numberOfCells}, auto)`);
@@ -17,29 +45,56 @@ function createCells(numberOfCells){
     currentCells = numberOfCells;
 }
 
-function clearBoard(currentCells){
-    const container = document.querySelector(".container");
-    for(let i=0; i<currentCells; i++){
-        container.removeChild("div");
+function clearBoard(){
+    const container = document.getElementById("container");
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
 }
 
 
 let mouseDown = false;
 
-document.body.onmouseenter = () => {
+window.onmouseenter = () => {
     if(mouseDown === true){
         mouseDown = false;
     }
 }
-
-document.body.onmousedown = () =>{
+window.onmousedown = () =>{
     mouseDown = true;
 }
-document.body.onmouseup = () =>{
+window.onmouseup = () =>{
+    mouseDown = false;
+}
+
+document.removeEventListener("pointercancel", PointerEvent);
+
+window.onpointercancel = () =>{
     mouseDown = false;
 }
 
 let currentCells = 0;
 createCells(16);
-console.log(currentCells);
+
+const clearButton = document.querySelector("#clear");
+const changeSizeButton = document.querySelector("#changesize");
+
+clearButton.addEventListener("click", () => {
+    createCells(currentCells);
+});
+changeSizeButton.addEventListener("click", () => {
+    // let numberOfCells = Math.round(Number(prompt("Type x in to make x by x grid")));
+    // while (numberOfCells < 1 && numberOfCells > 100){
+    //     alert("Wrong value. Accepted values: 1-100.");
+    //     numberOfCells = Math.round(Number(prompt("Type x in to make x by x grid")));
+    // }
+    const numberOfCells = document.getElementById("newsize").value;
+    if(numberOfCells > 100){
+        alert("Maximum value is 100");
+        return;
+    }else if(numberOfCells < 1){
+        alert("Minimum value is 1");
+        return;
+    }
+    createCells(numberOfCells);
+});
